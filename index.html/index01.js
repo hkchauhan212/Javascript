9a6652b5297f3864a9bdb84da943f417bdd8a265
hkchauhan212 <chauhanharikrushankishorbhai@gmail.com>
2024-04-08 21:47:03 +0530

    index.html
    THIS CODE IS HOW TO  JAVASCRIPT CODE RUN IN VSCODE IN HTML.

---
+++ index.html/index01.js
@@ -0,0 +1,10 @@
+//  Three type of string in javascript
+//  console.log is used to print in console and in browser console .
+//  `backtick` is used to print in console and in browser console.
+//   `` is used to print in console and in browser console.
+
+console.log("Hello World");
+
+console.log('Hello World');
+
+console.log(`Hello World`);
